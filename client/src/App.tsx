0fc9b101b93e2724459cd155fd7ac207eefdf60f
Harsh Sharma <harshsharmarohta@gmail.com>
2026-06-02 20:48:52 +0530

    chore: test coderabbit integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+// Testing CodeRabbit Automated Review
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
